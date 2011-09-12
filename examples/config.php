bcaa5606b526bf2b1c892d037ba7524ee9f0bf46
Romans Malinovskis <r@agiletech.ie>
2011-09-12 01:01:06 +0100

    don't ignore config.php in subdirectories

---
+++ examples/config.php
@@ -0,0 +1,8 @@
+<?php
+
+// Relative path to your ATK directory
+$config['atk']['base_path']='/atk4-example/atk4/';
+$config['dsn']='mysql://root:root@localhost/examples';
+
+$config['url_postfix']='';
+$config['url_prefix']='';
